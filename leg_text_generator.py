5f47cdf4a6f459c66878d5442608092d54b4aa74
qstin <justinjeffreyp@gmail.com>
2016-03-03 20:38:04 -0700

    finalize operations

--- leg_text_generator.py
+++ leg_text_generator.py
@@ -2,7 +2,7 @@ from urllib.request import urlopen
 import json
 import base64
 from bs4 import BeautifulSoup
-#Modify this script for readability in json parsing
+
 def getBillId():
     r = urlopen("https://api.legiscan.com/?key=2d28553a502d7fed3b68863b2f592f19&op=getMasterList&state=AZ").read().decode('utf-8')
     json_obj = json.loads(r)
@@ -22,10 +22,13 @@ def getBillId():
 
         #iterate to the bill key
         #get the doc_id to append to the API call
-        bill_num = bill.get("bill").get("bill_number")
+        try:
+            bill_num = bill.get("bill").get("bill_number")
+        except AttributeError:
+            bill_num = "bill" + str(num)
         doc_id = bill.get("bill").get("texts")[0].get("doc_id")
         #append the doc_id to the API call and convert results to unicode string
-        searchId = urlopen('https://api.legiscan.com/?key=2d28553a502d7fed3b68863b2f592f19&op=getBillText&id='+str(doc_id)).read().decode('utf-8')
+        searchId = urlopen('https://api.legiscan.com/?key=2d28553a502d7fed3b68863b2f592f19&op=getBillText&id='+str(doc_id)).read().decode()
 
         #create json object with API data
         resultsId = json.loads(searchId)
@@ -39,15 +42,17 @@ def getBillId():
         #once decoded, the text is in an HTML string, use bs4 to
         #make it parsable
         bsObj2 = BeautifulSoup(decodedResults)
+        for p in bsObj2.find_all('p'):
+            if p.string:
+                p.string.replace_with(p.string.strip())
         bsObj2.style.decompose()
 
         #strip white space, encode in ascii and remove non-printing characters
-        htmlText = str(bsObj2.getText(strip=True).encode('ascii',
-        'ignore')).replace("\\r\\n", "")
+        htmlText = str(bsObj2.getText()
 
         #print each instance of htmlText to a unique file
-        f = open("/media/qstin/var1/azcir/bills/" + str(bill_num) + ".txt", "w")
-        f.write(htmlText)
+        f = open("data/" + str(bill_num) + ".txt", "wb")
+        f.write(htmlText.encode("ascii", error="ignore")
         f.close()
         ++num
 
@@ -58,7 +63,7 @@ def getBills(bill_list):
 
     complete_bill_list = []
     for i in bill_list:
-        billUrl = urlopen("https://api.legiscan.com/?key=2d28553a502d7fed3b68863b2f592f19&op=getBill&id="+str(i)).read().decode("utf-8")
+        billUrl = urlopen("https://api.legiscan.com/?key=2d28553a502d7fed3b68863b2f592f19&op=getBill&id="+str(i)).read().decode()
         json_obj = json.loads(billUrl)
         complete_bill_list.append(json_obj)
 
